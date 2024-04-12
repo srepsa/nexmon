5bebf84f0a4d7323c42e36b3ff4c4b705d497c44
Jim Aspers <jim.aspers@me.com>
2024-04-12 17:44:50 +0200

    added rom_extraction project for bcm89359, tested OK

---
+++ patches/bcm89359/9_40_112_1_r712207_CY/rom_extraction/src/patch.c
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *                                                                         *
+ *          ###########   ###########   ##########    ##########           *
+ *         ############  ############  ############  ############          *
+ *         ##            ##            ##   ##   ##  ##        ##          *
+ *         ##            ##            ##   ##   ##  ##        ##          *
+ *         ###########   ####  ######  ##   ##   ##  ##    ######          *
+ *          ###########  ####  #       ##   ##   ##  ##    #    #          *
+ *                   ##  ##    ######  ##   ##   ##  ##    #    #          *
+ *                   ##  ##    #       ##   ##   ##  ##    #    #          *
+ *         ############  ##### ######  ##   ##   ##  ##### ######          *
+ *         ###########    ###########  ##   ##   ##   ##########           *
+ *                                                                         *
+ *            S E C U R E   M O B I L E   N E T W O R K I N G              *
+ *                                                                         *
+ * This file is part of NexMon.                                            *
+ *                                                                         *
+ * Copyright (c) 2016 NexMon Team                                          *
+ *                                                                         *
+ * NexMon is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation, either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * NexMon is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with NexMon. If not, see <http://www.gnu.org/licenses/>.          *
+ *                                                                         *
+ **************************************************************************/
+
+#pragma NEXMON targetregion "patch"
+
+#include <firmware_version.h>   // definition of firmware version macros
+#include <patcher.h>
+
+// we can place this in a region that seemingly has at least 0x100 bytes unused
+__attribute__((at(0x180AC0, "", CHIP_VER_BCM4359, FW_VER_9_40_112_1_r712207_CY)))
+__attribute__((optimize("O0")))
+void
+copy_hook(void) {
+	int *ram_dest = (int *) 0x185000;
+	int *rom_src = (int *) 0;
+
+    while(rom_src <= (int *) (ROMSIZE)) {
+        *ram_dest++ = *rom_src++;
+    }
+
+    while(1);
+}
+
+__attribute__((at(0x1830D0, "", CHIP_VER_BCM4359, FW_VER_9_40_112_1_r712207_CY)))
+GenericPatch4(copy_hook, copy_hook + 1);
